e20c45e3ecfac5158fd931dba6c357f3b48816f7
sanjaygadifakeu <sanjayyy19official@gmail.com>
2025-07-31 13:23:56 +0530

    finished off Git Actions nd Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S105</h1>
       <h1>CI/CD DevOps Machaw!</h1>
       <p>This is a simple homepage deployed on GitHub Actions  using Vite.</p>
+      <h1>Done with Git Pages & Actions</h1>
     </div>
   );
 }
